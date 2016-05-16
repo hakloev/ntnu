fe863b934ffdd2c851609ef5e2fce75840180b4f
Håkon Ødegård Løvdal <me@hakloev.no>
2016-05-16 15:20:58 +0200

    ai

--- tdt4171_metoder/ex5/backprop_skeleton.py
+++ tdt4171_metoder/ex5/backprop_skeleton.py
@@ -10,22 +10,22 @@ def log_func(x):
 
 # The derivative of the transfer function, g'(x)
 def log_func_derivative(x):
-    return math.exp(-x)/(pow(math.exp(-x)+1,2))
+    return math.exp(-x) / (pow(math.exp(-x) + 1, 2))
 
 
 def random_float(low, high):
-    return random.random()*(high-low) + low
+    return random.random() * (high - low) + low
 
 
 # Initializes a matrix of all zeros
 def make_matrix(i, j):
     m = []
     for i in range(i):
-        m.append([0]*j)
+        m.append([0] * j)
     return m
 
 
-class NeuralNEt(object): # Neural Network
+class NeuralNet(object):  # Neural Network
 
     def __init__(self, num_inputs, num_hidden, learning_rate=0.001):
         # Inputs: number of input and hidden nodes. Assuming a single output node.
@@ -36,14 +36,14 @@ class NeuralNEt(object): # Neural Network
         # Current activation levels for nodes (in other words, the nodes' output value)
         self.input_activation = [1.0] * self.num_inputs
         self.hidden_activations = [1.0] * self.num_hidden
-        self.output_activation = 1.0 # Assuming a single output.
+        self.output_activation = 1.0  # Assuming a single output.
         self.learning_rate = learning_rate
 
         # Create weights
         # A matrix with all weights from input layer to hidden layer
         self.weights_input = make_matrix(self.num_inputs, self.num_hidden)
-        #A list with all weights from hidden layer to the single output neuron.
-        self.weights_output = [0 for i in range(self.num_hidden)]# Assuming single output
+        # A list with all weights from hidden layer to the single output neuron.
+        self.weights_output = [0] * self.num_hidden  # Assuming single output
         # set them to random vaules
         for i in range(self.num_inputs):
             for j in range(self.num_hidden):
@@ -68,22 +68,22 @@ class NeuralNEt(object): # Neural Network
             raise ValueError('wrong number of inputs')
 
         # input activations
-        self.prev_input_activations=copy.deepcopy(self.input_activation)
+        self.prev_input_activations = copy.deepcopy(self.input_activation)
         for i in range(self.num_inputs-1):
             self.input_activation[i] = inputs[i]
-        self.input_activation[-1] = 1 #Set bias node to -1.
+        self.input_activation[-1] = 1  # Set bias node to -1.
 
         # hidden activations
-        self.prev_hidden_activations=copy.deepcopy(self.hidden_activations)
+        self.prev_hidden_activations = copy.deepcopy(self.hidden_activations)
         for j in range(self.num_hidden):
             sum_ = 0.0
             for i in range(self.num_inputs):
-                #print self.ai[i] ," * " , self.wi[i][j]
+                # print self.ai[i] ," * " , self.wi[i][j]
                 sum_ += self.input_activation[i] * self.weights_input[i][j]
             self.hidden_activations[j] = log_func(sum_)
 
         # output activations
-        self.prev_output_activation=self.output_activation
+        self.prev_output_activation = self.output_activation
         sum_ = 0.0
         for j in range(self.num_hidden):
             sum_ += self.hidden_activations[j] * self.weights_output[j]
@@ -91,16 +91,32 @@ class NeuralNEt(object): # Neural Network
         return self.output_activation
 
     def compute_output_delta(self):
-        # TODO: Implement the delta function for the output layer (see exercise text)
-        pass
+        # Equation 1 from the exercise text
+        p_ab = log_func(self.prev_output_activation - self.output_activation)
+        # Equation 2 from the exercise text
+        self.prev_delta_output = log_func_derivative(self.prev_output_activation) * (1 - p_ab)
+        # Equation 3 from the exercise text
+        self.delta_output = log_func_derivative(self.output_activation) * (1 - p_ab)
 
     def compute_hidden_delta(self):
-        # TODO: Implement the delta function for the hidden layer (see exercise text)
-        pass
+        # Equation 4 and 5 in the exercise text
+        for h in range(self.num_hidden):
+            # Equation 4
+            self.prev_delta_hidden[h] = log_func_derivative(self.prev_hidden_activations[h]) * self.weights_output[h] * (self.prev_delta_output - self.delta_output)
+            # Equation 5
+            self.delta_hidden[h] = log_func_derivative(self.hidden_activations[h]) * self.weights_output[h] * (self.prev_delta_output - self.delta_output)
 
     def update_weights(self):
-        # TODO: Update the weights of the network using the deltas (see exercise text)
-        pass
+        # Equation 6 in the exercise text, firstly for the input weights, and then for the output weights
+        for weight_i in range(self.num_inputs):
+            for weight_j in range(self.num_hidden):
+                self.weights_input[weight_i][weight_j] += self.learning_rate * \
+                                                          ((self.prev_delta_hidden[weight_j] * self.prev_input_activations[weight_i]) - \
+                                                          (self.delta_hidden[weight_j] * self.input_activation[weight_i]))
+        for weight_i in range(self.num_hidden):
+            self.weights_output[weight_i] += self.learning_rate * \
+                                             ((self.prev_hidden_activations[weight_i] * self.prev_delta_output) - \
+                                              (self.hidden_activations[weight_i] * self.delta_output))
 
     def backpropagate(self):
         self.compute_output_delta()
@@ -117,23 +133,38 @@ class NeuralNEt(object): # Neural Network
         print(self.weights_output)
 
     def train(self, patterns, iterations=1):
-        # TODO: Train the network on all patterns for a number of iterations.
-        # To measure performance each iteration: Run for 1 iteration, then count misordered pairs.
-        # TODO: Training is done  like this (details in exercise text):
-        # Propagate A
-        # Propagate B
         # Backpropagate
-        pass
+        error_during_iterations = list()
+        for iteration in range(iterations):
+            for a, b in patterns:
+                self.propagate(a.features)
+                self.propagate(b.features)
+                self.backpropagate()
+            error_during_iterations.append(self.count_misordered_pairs(patterns))
+        return error_during_iterations
 
     def count_misordered_pairs(self, patterns):
-        # TODO: Let the network classify all pairs of patterns. The highest output determines the winner.
-        # for each pair, do
-        # Propagate A
-        # Propagate B
-        # if A>B: A wins. If B>A: B wins
-        # if rating(winner) > rating(loser): numRight++
-        # else: numMisses++
-        # end of for
-        # TODO: Calculate the ratio of correct answers:
         # errorRate = numMisses/(numRight+numMisses)
-        pass
+        num_misses = 0
+        for a, b in patterns:
+            activation_a = self.propagate(a.features)
+            activation_b = self.propagate(b.features)
+            """
+            # Not necessary due to patterns already being sorted and reversed
+            if activation_a > activation_b:
+                if a.rating > b.rating:
+                    num_right += 1
+                else:
+                    num_misses += 1
+            else:
+                if b.rating > a.rating:
+                    num_right += 1
+                else:
+                    num_misses += 1
+            """
+
+            if activation_a < activation_b:
+                num_misses += 1
+
+        # print(num_misses, num_right, len(patterns))
+        return num_misses / len(patterns)
--- tdt4171_metoder/ex5/data_loader_skeleton.py
+++ tdt4171_metoder/ex5/data_loader_skeleton.py
@@ -1,4 +1,5 @@
 import backprop_skeleton as bp
+from matplotlib import pyplot as plt
 
 
 class DataInstance(object):
@@ -25,7 +26,7 @@ class DataHolder(object):
     def load_data(self, file):
         # Input: A file with the data.
         # Output: A dict mapping each query ID to the relevant documents, like this:
-        # dataset[queryID] = [dataInstance1, dataInstance2, ...]
+        # data_set[queryID] = [dataInstance1, dataInstance2, ...]
         data = open(file)
         data_set = {}
         for line in data:
@@ -43,51 +44,95 @@ class DataHolder(object):
             if qid in data_set.keys():
                 data_set[qid].append(di)
             else:
-                data_set[qid]=[di]
+                data_set[qid] = [di]
+        data.close()  # The file should be closed when done processing
         return data_set
 
 
-def run_ranker(training_set, test_set):
-    # TODO: Insert the code for training and testing your ranker here.
-    # Data holders for training and testset
+def generate_pairs(query, pattern_list):
+    """
+    Generates every possible pairs of the query set.
+    Sorts the list by rating, then reverses it so the highest number is first
+    :param query: The list of queries
+    :param pattern_list: The list to return, with every pair
+    :return: pattern_list
+    """
+    query = list(reversed(sorted(query, key=lambda x: x.rating)))
+    for first_instance in range(len(query) - 1):
+        for second_instance in range(first_instance + 1, len(query)):
+            if query[first_instance].rating == query[second_instance].rating:
+                continue
+            pattern_list.append((query[first_instance], query[second_instance]))
+
+
+def plot_history(*history, save=False):
+    """
+    Method for ploting x lists to a
+    :param history: A collection of tuples on the format (list, color, label)
+    :param save: Whether or not the plot should be saved
+    :return: None
+    """
+    plt.figure(1)
+    for hist, color, label in history:
+        plt.plot(hist, color, label=label)
+    # plt.axis([-5, len(history) + 10, min(history) - 0.001, max(history) + 0.001])
+    plt.ylim(0, 1)
+    plt.xlabel('Epochs')
+    plt.ylabel('Error Rate')
+    plt.legend()
+    if save:
+        plt.savefig('figures/history.png')
+    else:
+        plt.show(block=True)
+
+
+def run_ranker(training_set, test_set, iterations=25):
+    # Data holders for training- and test set
     dh_training = DataHolder(training_set)
     dh_testing = DataHolder(test_set)
 
     # Creating an ANN instance - feel free to experiment with the learning rate (the third parameter).
-    nn = bp.NN(46, 10, 0.001)
+    nn = bp.NeuralNet(46, 10, 0.001)
 
-    # TODO: The lists below should hold training patterns in this format: [(data1Features,data2Features),
-    #  (data1Features,data3Features), ... , (dataNFeatures,dataMFeatures)]
-    # TODO: The training set needs to have pairs ordered so the first item of the pair has a higher rating.
     training_patterns = []  # For holding all the training patterns we will feed the network
     test_patterns = []  # For holding all the test patterns we will feed the network
     for qid in dh_training.data_set.keys():
         # This iterates through every query ID in our training set
         data_instance = dh_training.data_set[qid]  # All data instances (query, features, rating) for query qid
-        # TODO: Store the training instances into the trainingPatterns array. Remember to store them as pairs,
-        # where the first item is rated higher than the second.
-        # TODO: Hint: A good first step to get the pair ordering right, is to sort the instances based on their
-        # rating for this query. (sort by x.rating for each x in dataInstance)
+        generate_pairs(data_instance, training_patterns)
 
     for qid in dh_testing.data_set.keys():
         # This iterates through every query ID in our test set
         data_instance = dh_testing.data_set[qid]
-        # TODO: Store the test instances into the testPatterns array, once again as pairs.
-        # TODO: Hint: The testing will be easier for you if you also now order the pairs -
-        # it will make it easy to see if the ANN agrees with your ordering.
+        generate_pairs(data_instance, test_patterns)
 
     # Check ANN performance before training
-    nn.count_misordered_pairs(test_patterns)
-    for i in range(25):
+    test_errors = [nn.count_misordered_pairs(test_patterns)]
+    training_erros = [nn.count_misordered_pairs(training_patterns)]
+    print('Starting training of Neural Net')
+    for i in range(iterations):
         # Running 25 iterations, measuring testing performance after each round of training.
         # Training
-        nn.train(training_patterns, iterations=1)
+        test_history_for_epoch = nn.train(training_patterns, iterations=1)
         # Check ANN performance after training.
-        nn.count_misordered_pairs(test_patterns)
+        error_epoch_test = nn.count_misordered_pairs(test_patterns)
+        error_epoch_training = nn.count_misordered_pairs(training_patterns)
+        test_errors.append(error_epoch_test)
+        training_erros.append(error_epoch_training)
+        print('Error rate during iteration %i: %s' % (i + 1, str(error_epoch_test)))
 
-    # TODO: Store the data returned by countMisorderedPairs and plot it,
-    # showing how training and testing errors develop.
-
+    return test_errors, training_erros
 
 if __name__ == '__main__':
-    run_ranker("train.txt", "test.txt")
+    num_of_iterations = 1  # The number of iterations to run run_ranker (the number of neural nets to create)
+    num_of_net_epochs = 20  # The number of epochs for each network to run
+    complete_test_history, complete_train_history = run_ranker('data/train.txt', 'data/test.txt', iterations=num_of_net_epochs)
+    for iteration in range(num_of_iterations - 1):
+        test_history, train_history = run_ranker('data/train.txt', 'data/test.txt', iterations=num_of_net_epochs)
+        complete_test_history = list(map(lambda x, y: x + y, complete_test_history, test_history))
+        complete_train_history = list(map(lambda x, y: x + y, complete_train_history, train_history))
+
+    complete_test_history = list(map(lambda x: x / num_of_iterations, complete_test_history))
+    complete_train_history = list(map(lambda x: x / num_of_iterations, complete_train_history))
+
+    plot_history((complete_test_history, 'b-', 'Testing'), (complete_train_history, 'r--', 'Training'))
